154f067319c0866cdf728c15bea31af388d10487
Rachael Mohl <rmohl@uoguelph.ca>
2022-09-14 16:27:39 -0400

    can now change navbar code in navbar.js, and affect how the navbar is displayed on every webpage

---
+++ navbar.js
@@ -0,0 +1,34 @@
+// code for navbar
+
+document.write(`
+
+<div class="navbar">
+
+    <div class="op1">
+        <a href="index.html"><button class="homeBtn">Home</button></a>
+    </div>
+
+    <div class="op2">
+        <a href="media/resume.pdf"><button class="resumeBtn">Resume</button></a>
+    </div>
+
+    <div class="dropdown">
+        <button class="dropbtn">Co-op Work Term Reports</button>
+        <div class="dropdown-content">
+            <a href="s21-overview.html">Summer 2021</a>
+            <a href="f21-overview.html">Fall 2021</a>
+            <a href="s22-overview.html">Summer 2022</a>
+        </div>
+    </div>
+
+    <div class="dropdown2">
+        <button class="dropbtn2">Links</button>
+        <div class="dropdown-content2">
+            <a href="https://github.com/rmohl" target="_blank">GitHub</a>
+            <a href="https://www.linkedin.com/in/rachael-mohl" target="_blank">LinkedIn</a>
+        </div>
+    </div>
+
+</div>
+
+`);
