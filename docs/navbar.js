8d291d8910737be95fe9bc284a5ab9d3f7fb7d8d
Rachael Mohl <rmohl@uoguelph.ca>
2023-05-16 11:30:24 -0400

    simplified css

--- docs/navbar.js
+++ docs/navbar.js
@@ -5,15 +5,15 @@ document.write(`
 <div class="navbar">
 
     <div class="op1">
-        <a href="index.html"><button class="homeBtn">Home</button></a>
+        <a href="index.html"><button class="navBtn">Home</button></a>
     </div>
 
     <div class="op2">
-        <a href="media/resume.pdf"><button class="resumeBtn">Resume</button></a>
+        <a href="media/resume.pdf"><button class="navBtn">Resume</button></a>
     </div>
 
     <div class="dropdown">
-        <button class="dropbtn">Co-op Work Term Reports</button>
+        <button class="navBtn">Co-op Work Term Reports</button>
         <div class="dropdown-content">
             <a href="s21-overview.html">Summer 2021</a>
             <a href="f21-overview.html">Fall 2021</a>
@@ -22,7 +22,7 @@ document.write(`
     </div>
 
     <div class="dropdown2">
-        <button class="dropbtn2">Links</button>
+        <button class="navBtn">Links</button>
         <div class="dropdown-content2">
             <a href="https://github.com/rmohl" target="_blank">GitHub</a>
             <a href="https://www.linkedin.com/in/rachael-mohl" target="_blank">LinkedIn</a>
